68db051307f15c558911c9d1de9ecb150fd50392
vladpaun1 <vlad.paun06@gmail.com>
2025-10-24 01:18:14 +0200

    Chapter 7

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='bg-white'>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
