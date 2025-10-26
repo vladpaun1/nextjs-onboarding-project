82f7bf4bb56a16d7e4ef86b274e5933017be033c
vladpaun1 <vlad.paun06@gmail.com>
2025-10-26 18:31:34 +0100

    Chapter 16

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,4 +1,11 @@
-export default function Page() {
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+ 
+
+export default function Page() {   
     return (
         <p>Customers Page</p>
     );
