c530a3be00f4994662138cd17bcd62df5dd89e13
vladpaun1 <vlad.paun06@gmail.com>
2025-10-26 18:20:09 +0100

    Chapter 15

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -3,6 +3,29 @@ import { z } from 'zod';
 import postgress from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData
+) {
+    try {
+        await signIn('credentials', formData)
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
+
 
 const sql = postgress(process.env.POSTGRES_URL!, { ssl: 'require' });
 
